8e200e9fdc8e21e71dfe5f2a401d85ca250078a5
AndrewEfimovich <113019976+Maloo22@users.noreply.github.com>
2023-06-05 14:58:41 +0300

    +5sec delay

--- src/test/java/testcases/TestCase.java
+++ src/test/java/testcases/TestCase.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class TestCase {
 
     @Test
@@ -15,6 +17,13 @@ public class TestCase {
 
         driver.get("https://ttrsonline.com/Account/LogOn");
         driver.manage().window().maximize();
+        try {
+            TimeUnit.SECONDS.sleep(5); // Задержка в 5 секунду
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         driver.close();
+
     }
 }
